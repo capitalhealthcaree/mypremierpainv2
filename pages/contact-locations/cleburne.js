17957d708ffe657fe8327bd4f5c4606320114baf
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-08-03 16:37:45 +0500

    update canonical celeberum

--- pages/contact-locations/cleburne.js
+++ pages/contact-locations/cleburne.js
@@ -5,15 +5,18 @@ import PageBanner from "../../components/Common/PageBanner";
 import ContactInfo from "../../components/Contact/ContactInfo";
 import Footer from "../../components/_App/Footer";
 import AppointmentForm from "../../components/Appointment/AppointmentForm";
+import { useRouter } from "next/router";
 
 const Contact = () => {
+  const router = useRouter();
+
   return (
     <>
       <Head>
         <title>Pain Management Doctors in Cleburne, TX</title>
         <link
           rel="canonical"
-          href={`${process.env.NEXT_PUBLIC_URL_DOMAIN}contact-locations/cleburne/`}
+          content={`${process.env.NEXT_PUBLIC_URL_DOMAIN}${router.asPath}`}
         />
         <meta
           name="description"
