029a14bc239bec9cf3d75526a3df963976d11682
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-09-18 08:53:57 -0700

    remoive redirects of some url and add it on spam urls

--- next.config.js
+++ next.config.js
@@ -18,58 +18,6 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/blog/list-some-of-the-best-exercises-for-fractured-patella/",
-        destination:
-          "/blog/leg-pain/list-some-of-the-best-exercises-for-fractured-patella/",
-        permanent: true,
-      },
-      {
-        source: "/blog/9-causes-you-feel-pain-in-the-front-of-the-neck/",
-        destination:
-          "/blog/neck-pain/9-causes-you-feel-pain-in-the-front-of-the-neck/",
-        permanent: true,
-      },
-      {
-        source: "/conditions-we-treat/educational-videos/",
-        destination: "/educational-videos/",
-        permanent: true,
-      },
-      {
-        source: "/meet-our-team/conditions-we-treat/sports-injury/",
-        destination: "/conditions-we-treat/sports-injury/",
-        permanent: true,
-      },
-      {
-        source: "/meet-our-team/conditions-we-treat/headaches/",
-        destination: "/conditions-we-treat/headaches/",
-        permanent: true,
-      },
-      {
-        source: "/meet-our-team/conditions-we-treat/neck-pain/",
-        destination: "/conditions-we-treat/neck-pain/",
-        permanent: true,
-      },
-      {
-        source: "/meet-our-team/conditions-we-treat/joint-pain/",
-        destination: "/conditions-we-treat/joint-pain/",
-        permanent: true,
-      },
-      {
-        source: "/meet-our-team/educational-videos/",
-        destination: "/educational-videos/",
-        permanent: true,
-      },
-      {
-        source: "/treatments/meet-our-team/",
-        destination: "/meet-our-team/",
-        permanent: true,
-      },
-      {
-        source: "/meet-our-team/reviews/",
-        destination: "/reviews/",
-        permanent: true,
-      },
-      {
         source: "/treatments/conditions-we-treat/car-accident-injury/",
         destination: "/conditions-we-treat/car-accident-injury/",
         permanent: true,
@@ -134,31 +82,26 @@ const nextConfig = {
         destination: "/video-testimonials/",
         permanent: true,
       },
-
       {
         source: "/treatments/conditions-we-treat/back-pain/",
         destination: "/conditions-we-treat/back-pain/",
         permanent: true,
       },
-
       {
         source: "/treatments/contact-locations/richardson/",
         destination: "/contact-locations/richardson/",
         permanent: true,
       },
-
       {
         source: "/treatments/about/careers/",
         destination: "/about/",
         permanent: true,
       },
-
       {
         source: "/treatments/conditions-we-treat/sports-injury/",
         destination: "/conditions-we-treat/sports-injury/",
         permanent: true,
       },
-
       {
         source: "/treatments/contact-locations/farmers-branch/",
         destination: "/contact-locations/farmers-branch/",
@@ -169,13 +112,11 @@ const nextConfig = {
         destination: "/conditions-we-treat/knee-pain/",
         permanent: true,
       },
-
       {
         source: "/treatments/conditions-we-treat/headaches/",
         destination: "/conditions-we-treat/headaches/",
         permanent: true,
       },
-
       {
         source: "/treatments/conditions-we-treat/neck-pain/",
         destination: "/conditions-we-treat/neck-pain/",
@@ -191,10 +132,14 @@ const nextConfig = {
         destination: "/contact-locations/",
         permanent: true,
       },
-
       {
         source: "/treatments/patient-forms/",
         destination: "/treatments/",
+        permanent: true,
+      },
+      {
+        source: "/treatments/meet-our-team/",
+        destination: "/meet-our-team/",
         permanent: true,
       },
 
@@ -223,13 +168,11 @@ const nextConfig = {
         destination: "/contact-locations/waxahachie/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/conditions-we-treat/sports-injury/",
         destination: "/conditions-we-treat/sports-injury/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/contact-locations/hillsboro/",
         destination: "/contact-locations/hillsboro/",
@@ -240,61 +183,51 @@ const nextConfig = {
         destination: "/educational-videos/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/conditions-we-treat/headaches/",
         destination: "/conditions-we-treat/headaches/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/conditions-we-treat/knee-pain/",
         destination: "/conditions-we-treat/knee-pain/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/patient-info/",
         destination: "/contact-locations/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/contact-locations/lancaster/",
         destination: "/contact-locations/lancaster/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/contact-locations/fort-worth/",
         destination: "/contact-locations/fort-worth/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/conditions-we-treat/",
         destination: "/conditions-we-treat/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/conditions-we-treat/neck-pain/",
         destination: "/conditions-we-treat/neck-pain/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/conditions-we-treat/back-pain/",
         destination: "/conditions-we-treat/back-pain/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/reviews/",
         destination: "/reviews/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/contact-locations/",
         destination: "/contact-locations/",
@@ -310,19 +243,16 @@ const nextConfig = {
         destination: "/video-testimonials/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/conditions-we-treat/car-accident-injury/",
         destination: "/conditions-we-treat/car-accident-injury/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/about/",
         destination: "/about/",
         permanent: true,
       },
-
       {
         source: "/contact-locations/conditions-we-treat/joint-pain/",
         destination: "/conditions-we-treat/joint-pain/",
@@ -334,61 +264,6 @@ const nextConfig = {
         permanent: true,
       },
 
-      {
-        source: "/conditions-we-treat/contact-locations/corsicana/",
-        destination: "/contact-locations/corsicana/",
-        permanent: true,
-      },
-
-      {
-        source: "/conditions-we-treat/contact-locations/north-richland-hills/",
-        destination: "/contact-locations/north-richland-hills/",
-        permanent: true,
-      },
-      {
-        source: "/conditions-we-treat/contact-locations/farmers-branch/",
-        destination: "/contact-locations/farmers-branch/",
-        permanent: true,
-      },
-      {
-        source: "/conditions-we-treat/contact-locations/richardson/",
-        destination: "/contact-locations/richardson/",
-        permanent: true,
-      },
-
-      {
-        source: "/conditions-we-treat/contact-locations/hillsboro/",
-        destination: "/contact-locations/hillsboro/",
-        permanent: true,
-      },
-      {
-        source: "/conditions-we-treat/reviews/",
-        destination: "/reviews/",
-        permanent: true,
-      },
-      {
-        source: "/conditions-we-treat/contact-locations/waxahachie/",
-        destination: "/contact-locations/waxahachie/",
-        permanent: true,
-      },
-
-      {
-        source: "/conditions-we-treat/contact-locations/fort-worth/",
-        destination: "/contact-locations/fort-worth/",
-        permanent: true,
-      },
-
-      {
-        source: "/conditions-we-treat/contact-locations/",
-        destination: "/contact-locations/",
-        permanent: true,
-      },
-
-      {
-        source: "/conditions-we-treat/contact-locations/lancaster/",
-        destination: "/contact-locations/lancaster/",
-        permanent: true,
-      },
       {
         source: "/meet-our-team/contact-locations/corsicana/",
         destination: "/contact-locations/",
@@ -435,37 +310,33 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: "/news/contact-locations/hillsboro/",
-        destination: "/news/",
-        permanent: true,
-      },
-
-      {
-        source: "/about/contact-locations/fort-worth/",
-        destination: "/about/",
-        permanent: true,
-      },
-
-      {
-        source: "/about/contact-locations/richardson/",
-        destination: "/about/",
-        permanent: true,
-      },
-
-      {
-        source: "/about/contact-locations/hillsboro/",
-        destination: "/about/",
+        source: "/meet-our-team/conditions-we-treat/sports-injury/",
+        destination: "/conditions-we-treat/sports-injury/",
         permanent: true,
       },
       {
-        source: "/news/contact-locations/richardson/",
-        destination: "/news/",
+        source: "/meet-our-team/conditions-we-treat/headaches/",
+        destination: "/conditions-we-treat/headaches/",
         permanent: true,
       },
-
       {
-        source: "/news/contact-locations/fort-worth/",
-        destination: "/news/",
+        source: "/meet-our-team/conditions-we-treat/neck-pain/",
+        destination: "/conditions-we-treat/neck-pain/",
+        permanent: true,
+      },
+      {
+        source: "/meet-our-team/conditions-we-treat/joint-pain/",
+        destination: "/conditions-we-treat/joint-pain/",
+        permanent: true,
+      },
+      {
+        source: "/meet-our-team/educational-videos/",
+        destination: "/educational-videos/",
+        permanent: true,
+      },
+      {
+        source: "/meet-our-team/reviews/",
+        destination: "/reviews/",
         permanent: true,
       },
       {
@@ -489,11 +360,6 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: "/car-accident-injury/",
-        destination: "/conditions-we-treat/car-accident-injury/",
-        permanent: true,
-      },
-      {
         source: "/meet-our-team/meet-our-team/",
         destination: "/meet-our-team/",
         permanent: true,
@@ -506,6 +372,12 @@ const nextConfig = {
       {
         source: "/meet-our-team/video-testimonials/",
         destination: "/video-testimonials/",
+        permanent: true,
+      },
+
+      {
+        source: "/conditions-we-treat/educational-videos/",
+        destination: "/educational-videos/",
         permanent: true,
       },
       {
@@ -523,6 +395,108 @@ const nextConfig = {
         destination: "/meet-our-team/",
         permanent: true,
       },
+      {
+        source: "/conditions-we-treat/contact-locations/corsicana/",
+        destination: "/contact-locations/corsicana/",
+        permanent: true,
+      },
+      {
+        source: "/conditions-we-treat/contact-locations/north-richland-hills/",
+        destination: "/contact-locations/north-richland-hills/",
+        permanent: true,
+      },
+      {
+        source: "/conditions-we-treat/contact-locations/farmers-branch/",
+        destination: "/contact-locations/farmers-branch/",
+        permanent: true,
+      },
+      {
+        source: "/conditions-we-treat/contact-locations/richardson/",
+        destination: "/contact-locations/richardson/",
+        permanent: true,
+      },
+
+      {
+        source: "/conditions-we-treat/contact-locations/hillsboro/",
+        destination: "/contact-locations/hillsboro/",
+        permanent: true,
+      },
+      {
+        source: "/conditions-we-treat/reviews/",
+        destination: "/reviews/",
+        permanent: true,
+      },
+      {
+        source: "/conditions-we-treat/contact-locations/waxahachie/",
+        destination: "/contact-locations/waxahachie/",
+        permanent: true,
+      },
+      {
+        source: "/conditions-we-treat/contact-locations/fort-worth/",
+        destination: "/contact-locations/fort-worth/",
+        permanent: true,
+      },
+      {
+        source: "/conditions-we-treat/contact-locations/",
+        destination: "/contact-locations/",
+        permanent: true,
+      },
+      {
+        source: "/conditions-we-treat/contact-locations/lancaster/",
+        destination: "/contact-locations/lancaster/",
+        permanent: true,
+      },
+
+      // {
+      //   source: "/about/contact-locations/fort-worth/",
+      //   destination: "/about/",
+      //   permanent: true,
+      // },
+      // {
+      //   source: "/about/contact-locations/richardson/",
+      //   destination: "/about/",
+      //   permanent: true,
+      // },
+      // {
+      //   source: "/about/contact-locations/hillsboro/",
+      //   destination: "/about/",
+      //   permanent: true,
+      // },
+
+      // {
+      //   source: "/news/contact-locations/hillsboro/",
+      //   destination: "/news/",
+      //   permanent: true,
+      // },
+      // {
+      //   source: "/news/contact-locations/richardson/",
+      //   destination: "/news/",
+      //   permanent: true,
+      // },
+      // {
+      //   source: "/news/contact-locations/fort-worth/",
+      //   destination: "/news/",
+      //   permanent: true,
+      // },
+
+      // {
+      //   source: "/blog/list-some-of-the-best-exercises-for-fractured-patella/",
+      //   destination:
+      //     "/blog/leg-pain/list-some-of-the-best-exercises-for-fractured-patella/",
+      //   permanent: true,
+      // },
+      // {
+      //   source: "/blog/9-causes-you-feel-pain-in-the-front-of-the-neck/",
+      //   destination:
+      //     "/blog/neck-pain/9-causes-you-feel-pain-in-the-front-of-the-neck/",
+      //   permanent: true,
+      // },
+
+      // {
+      //   source: "/car-accident-injury/",
+      //   destination: "/conditions-we-treat/car-accident-injury/",
+      //   permanent: true,
+      // },
     ];
   },
 };
