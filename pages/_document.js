ca49c9ff1a1f95174f75d3bf4a69b11e90603916
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-11-29 06:35:34 -0800

    add chat id

--- pages/_document.js
+++ pages/_document.js
@@ -40,7 +40,7 @@ class MyDocument extends Document {
             defer
             dangerouslySetInnerHTML={{
               __html: `
-                var continuallySettings = { appID: "ne9y68v92yzk" };
+                var continuallySettings = { appID: "6wkym7jg3ye2" };
                 var script = document.createElement('script');
                 script.src = "https://cdn-app.continual.ly/js/embed/continually-embed.latest.min.js";
                 script.async = true;
