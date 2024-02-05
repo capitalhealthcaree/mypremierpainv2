8064c3f9e9e011e2804a384a9378abd9a904a317
capitalhealthcaree <premierpatientappointment@gmail.com>
2024-02-05 14:27:13 -0800

    assdd

--- components/FindLocation/index.js
+++ components/FindLocation/index.js
@@ -38,7 +38,7 @@ const MapComponent = ({ specificLocation }) => {
 
   return (
     <LoadScript googleMapsApiKey="AIzaSyCpBNr90j-cwpy4VemT03i_-E878KrDnZE">
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={8}>
         {locations.map((location, index) => (
           <Marker key={index} position={location} />
         ))}
