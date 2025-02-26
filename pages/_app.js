fcb02486f425886cee6fcdee611410eb4db34d28
capitalhealthcaree <premierpatientappointment@gmail.com>
2025-02-26 20:23:46 +0500

    add new chat bot id

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import "../node_modules/react-modal-video/css/modal-video.min.css";
 import "react-accessible-accordion/dist/fancy-example.css";
 import "swiper/css";
 import "swiper/css/bundle";
+import Script from "next/script";
 
 // Global Style
 import "../styles/style.css";
@@ -20,6 +21,19 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Script
+        id="messenger-widget-b"
+        src="https://cdn.botpenguin.com/website-bot.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          if (window.BotPenguin) {
+            window.BotPenguin.init(
+              "67bf286b4deb655bc592f3bb",
+              "67bf283eb0a5145cacb98a41"
+            );
+          }
+        }}
+      />
     </Layout>
   );
 };
