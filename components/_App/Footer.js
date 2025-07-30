62241aeaa626c0ae3e2cad613fe84306e6a3e946
capitalhealthcaree <premierpatientappointment@gmail.com>
2025-07-31 02:21:21 +0500

    update linkedin

--- components/_App/Footer.js
+++ components/_App/Footer.js
@@ -93,7 +93,8 @@ const Footer = () => {
                     </li> */}
                     <li style={{ marginRight: "10px" }}>
                       <a
-                        href="https://www.linkedin.com/in/premier-pain-centers-08582018a/"
+                        // href="https://www.linkedin.com/in/premier-pain-centers-08582018a/"
+                        href="https://www.linkedin.com/company/premier-pain-centers-in-dallas/"
                         // href="https://www.linkedin.com/in/premier-pain-centers/"
                         aria-label="Linkedin"
                         target="_blank"
