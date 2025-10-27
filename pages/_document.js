6bab1d3d0a4ab8f7f5b56bdf3003dfdf56f11dcf
capitalhealthcaree <premierpatientappointment@gmail.com>
2025-10-28 01:17:49 +0500

    chat id done

--- pages/_document.js
+++ pages/_document.js
@@ -54,7 +54,7 @@ class MyDocument extends Document {
             defer
             dangerouslySetInnerHTML={{
               __html: `
-                var continuallySettings = { appID: "6wkymeme3ye2" };
+                var continuallySettings = { appID: "p87rwvggqyq9" };
                 var script = document.createElement('script');
                 script.src = "https://cdn-app.continual.ly/js/embed/continually-embed.latest.min.js";
                 script.async = true;
